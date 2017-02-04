3d39eda124b93594af3a978109a8408c1ac3419e
SilanHe <silanhe@outlook.com>
2017-02-04 13:25:45 -0500

    webpack

---
+++ src/js/client.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./components/Layout";
+
+
+const posts = document.getElementById('posts');
+
+ReactDOM.render(<Layout/ >, posts);
