8b9715a0f6b0b474112d2aa9d68db837e1282bdd
SilanHe <silanhe@outlook.com>
2017-02-05 02:05:26 -0500

    removed hi

---
+++ src/js/components/Data.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default class Data extends React.Component {
+	constructor {
+		super();
+		
+	}
+	render() {
+		return (
+			<div>
+				<span>
+
+				</span>
+			</div>
+		);
+	}
+}
